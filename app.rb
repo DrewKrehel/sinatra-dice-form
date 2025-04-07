4be70d0f1ebbfcc46854643bd589da04e67896c9
Drew Krehel <drewkrehel@gmail.com>
2025-04-07 17:46:45 +0000

    save 1

--- app.rb
+++ app.rb
@@ -5,14 +5,26 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/:die1/:die2") do
-  @num_die1 = params.fetch("die1").to_i
-  @num_die2 = params.fetch("die2").to_i
+get("/process_roll") do
+  @num_dice = params.fetch("dice").to_i
+  @num_sides = params.fetch("sides").to_i
 
-  @dice = Array.new(@num_die1) {rand(1..@num_die2)}
+  @dice = Array.new(@num_dice) {rand(1..@num_sides)}
   @sum = @dice.sum
 
   @outcome = "You rolled a total of #{@sum}."
 
   erb(:flexible)
 end
+
+# get("/dice/:die1/:die2") do
+#   @num_dice = params.fetch("die1").to_i
+#   @num_side = params.fetch("die2").to_i
+
+#   @dice = Array.new(@num_die1) {rand(1..@num_die2)}
+#   @sum = @dice.sum
+
+#   @outcome = "You rolled a total of #{@sum}."
+
+#   erb(:flexible)
+# end
