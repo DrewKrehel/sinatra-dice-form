0cef9c3ff126dbc344e204f4e776966030662d61
Drew Krehel <drewkrehel@gmail.com>
2025-04-07 17:03:09 +0000

    getting started

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/dice/:die1/:die2") do
+  @num_die1 = params.fetch("die1").to_i
+  @num_die2 = params.fetch("die2").to_i
+
+  @dice = Array.new(@num_die1) {rand(1..@num_die2)}
+  @sum = @dice.sum
+
+  @outcome = "You rolled a total of #{@sum}."
+
+  erb(:flexible)
 end
